247be02eb43f6ecdfbc697bd271a27306a932107
stefan judis <stefanjudis@gmail.com>
2019-02-19 21:36:24 +0100

    Fix function

--- functions/send-deploy-message.js
+++ functions/send-deploy-message.js
@@ -3,29 +3,32 @@ require('dotenv').config();
 const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, BOT_NUMBER } = process.env;
 const client = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
-client.messages
-  .list({
-    to: BOT_NUMBER
-  })
-  .then(messages => {
-    const numbers = [
-      ...messages.reduce((acc, msg) => {
-        acc.add(msg.from);
-        return acc;
-      }, new Set())
-    ];
+exports.handler = function(event, context, callback) {
+  client.messages
+    .list({
+      to: BOT_NUMBER
+    })
+    .then(messages => {
+      const numbers = [
+        ...messages.reduce((acc, msg) => {
+          acc.add(msg.from);
+          return acc;
+        }, new Set())
+      ];
 
-    return Promise.all(
-      numbers.map(number => {
-        client.messages.create({
-          from: BOT_NUMBER,
-          body: 'Site updated. 🎉',
-          to: number
-        });
-      })
-    );
-  })
-  .then(() => {
-    console.log('Coooool!');
-  })
-  .catch(e => console.log(e));
+      return Promise.all(
+        numbers.map(number => {
+          client.messages.create({
+            from: BOT_NUMBER,
+            body: 'Site updated. 🎉',
+            to: number
+          });
+        })
+      );
+    })
+    .then(() => {
+      console.log('Coooool!');
+      callback();
+    })
+    .catch(e => callback(e));
+};
