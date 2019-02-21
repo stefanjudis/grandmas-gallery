960c2077d04e0b52d89f74a8f3ff221ca8860f8f
stefan judis <stefanjudis@gmail.com>
2019-02-21 11:08:11 +0100

    Add form submission fn

---
+++ functions/submission-created.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, BOT_NUMBER } = process.env;
+const client = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+
+exports.handler = function(event, context, callback) {
+  client.messages
+    .list({
+      to: BOT_NUMBER
+    })
+    .then(messages => {
+      const numbers = [
+        ...messages.reduce((acc, msg) => {
+          acc.add(msg.from);
+          return acc;
+        }, new Set())
+      ];
+
+      return Promise.all(
+        numbers.map(number => {
+          client.messages.create({
+            from: BOT_NUMBER,
+            body: 'Grandma wants to get a picture!',
+            to: number
+          });
+        })
+      );
+    })
+    .then(() => {
+      console.log('Coooool!');
+      callback();
+    })
+    .catch(e => callback(e));
+};
